cc118bea395f2f1377ecc3fd7f8ac6a856fde3a3
xNikhil001 <naikn8113@gmail.com>
2022-04-17 16:03:35 +0530

    added search filter to navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,17 +1,41 @@
 import {useState} from 'react';
+import axios from 'axios';
 import Link from 'next/link';
 import {useRouter} from 'next/router';
 import { useSession, signIn, signOut } from "next-auth/react";
+import useSWR from 'swr';
+
+const fetcher = async (url) => {
+    const data = await axios.get(url).then((res)=>res.data.result);
+    return data;
+  };
 
 function Navbar(){
+  const url = `https://cp0099.herokuapp.com/api/coins`
   const { data: session } = useSession();
   const router = useRouter();
   const [isNavOpen,setIsNavOpen] = useState(false);
   const [openMenu,setOpenMenu] = useState(false);
+  const [openSearch,setOpenSearch] = useState(false);
+  const [coinName,setCoinName] = useState("");
   const path = router.pathname;
   const toggleNav = ()=>{
     setIsNavOpen(!isNavOpen);
+    setOpenSearch(false)
+    setOpenMenu(false)
   }
+  
+  const { data,error } = useSWR(url,fetcher);
+  
+  const handleChange = (e)=>{
+    setCoinName(e.target.value)
+  }
+  const items = (data || []);
+  const viewCoinInfo = (ID)=>{
+    router.push(`/coininfo/${ID}`);
+    setOpenSearch(false)
+    setOpenMenu(false)
+  };
     return(
       <nav className="flex justify-between w-full h-16 items-center shadow-3xl z-50 bg-[#6E6E6E] fixed top-0">
         <div className="absolute t-4 left-3"><Link href="/"><a className="text-3xl text-[#BCFD4C]">CoinParadise</a></Link></div>
@@ -24,13 +48,43 @@ function Navbar(){
         <div className={`md:hidden absolute top-4 right-5`} onClick={toggleNav}>
           <span className="text-3xl text-[#BCFD4C]">&#x2632;</span>
         </div>
-        <div className={`md:hidden absolute top-[26%] right-16`}>
-          <i className="fa-solid fa-circle-user text-3xl" onClick={()=>setOpenMenu(!openMenu)}></i>
+        
+        <div className={`md:hidden absolute top-[29%] right-16`}>
+          <i className="fa-solid fa-circle-user text-2xl" onClick={()=>{
+            setOpenMenu(!openMenu);
+            if(openSearch){
+              setOpenSearch(!openSearch);
+            }
+            }
+          }></i>
         </div>
-        <div className={`w-6/12 h-[10rem] bg-[#999999] flex flex-col absolute top-[82%] right-[16%] rounded-sm shadow-3xl ${openMenu ? "block" : "hidden"} items-center justify-center`}>
-          {session && (<div className="flex flex-col text-center"><span className="mb-4">Welcome,{session.user.name}!</span><button className="border border-[#BCFD4C] rounded-md w-8/12 mx-auto" onClick={()=>signOut()}>Sign Out</button></div>)}
-          {!session && (<button className="border border-[#BCFD4C] rounded-md w-5/12 mx-auto" onClick={()=>signIn()}>Sign In</button>)}
+        <div className={`w-6/12 h-[10rem] bg-[#999999] flex flex-col absolute top-[82%] right-[16%] rounded-sm shadow-3xl transition-all duration-300 ease-in-out ${openMenu ? "opacity-100" : "opacity-0"} items-center justify-center text-white`}>
+          {session && (<div className="flex flex-col text-center"><span className="mb-4">Welcome,{session.user.name}!</span><button className="border border-[#BCFD4C] rounded-md w-8/12 mx-auto p-1" onClick={()=>signOut()}>Sign Out</button></div>)}
+          {!session && (<button className="border border-[#BCFD4C] rounded-md w-5/12 mx-auto p-1" onClick={()=>signIn()}>Sign In</button>)}
         </div>
+       
+        <div className={`md:hidden absolute top-[29%] right-28`}>
+          <i className="fas fa-search text-2xl" onClick={()=>{
+            setOpenSearch(!openSearch);
+            if(openMenu){
+              setOpenMenu(!openMenu);
+            }
+            }
+          }></i>
+        </div>
+        <div className={`w-8/12 h-[20rem] bg-[#999999] flex flex-col absolute top-[82%] right-[16%] rounded-sm shadow-3xl transition-all duration-300 ease-in-out ${openSearch ? "opacity-100" : "opacity-0"}`}>
+          <input className="border w-full p-2 rounded-sm placeholder-gray-500 text-gray-500 outline-0 bg-gray-300" type="search" name="search" value={coinName} placeholder="search coin" onChange={handleChange} />
+          <div className="overflow-scroll">
+          {items.length !=0 && items.filter((val)=>{
+            if(coinName == ""){
+              return ""
+            }else if(val.name.toLowerCase().includes(coinName.toLowerCase())){
+              return val;
+            }
+          }).map((el)=>(<div className="w-11/12 mx-auto my-4 text-lg text-white" onClick={()=>viewCoinInfo(el._id)}>{el.name}</div>))}
+          </div>
+        </div>
+        
         <ul className={`w-full z-50 fixed top-0 h-screen md:hidden bg-black text-white text-center text-2xl ${isNavOpen? "translate-x-0" : "-translate-x-full"} transition-all duration-300`}>
           <li className="absolute top-4 left-5">
             <Link href="/">
