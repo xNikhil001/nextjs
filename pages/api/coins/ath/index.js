07bdf2193546f072f08690e84dfc74e2480c78f1
xNikhil001 <naikn8113@gmail.com>
2022-04-21 18:39:14 +0530

    added verification

--- pages/api/coins/ath/index.js
+++ pages/api/coins/ath/index.js
@@ -1,6 +1,18 @@
+import axios from 'axios';
+import jwt from 'jsonwebtoken';
+
 export default async function handler(req,res){
   const url = `https://cp0099.herokuapp.com/api/coins/ath`;
-  const getData = await fetch(url)
-  const data = await getData.json();
-  return res.status(200).send(data)
+  const loc = `http://localhost:8000/api/coins/ath`
+  const SECRET_KEY="rJGNX+ehxRvcsNEFERwU0lUbzOXsb9fXgjEIbCh5f8xh3UdYlKQ4JrLCKW0NEIFkP/aUpx6pUs6h6N98Hgu/lEfqo7BDs3EcQ1Dfu8rYr19mWzaVmOWdhSLX7PxGJFKO3hyafrw0yLOJoOZljcsyftwwWq7UafhNyhwF6PKKwk6POAUPdqZP41ux7eCSvhWTt5fWXmvldcgYemPqdY7j1FisGJq8zmP68jJ4WUUNYQy5jtYfyp0MFYrQzhHiNmX48ZkkcdeMXbybLvLBWUyK1P9+vzfotB4SNKQk5OsusI0c8sgmkOHm4G0yOv69/wwkqYmnmLtkiKjz9jjZoFLOyw=="
+    const token = jwt.sign({type:'verification'},SECRET_KEY,{algorithm:'HS256'})
+    const options = {
+      headers: {
+        'Content-Type':'application/json',
+        referer: 'https://nextjs-dusky-gamma.vercel.app/',
+        'verify-key': token
+      }
+    }
+  const data = await axios.get(url,options)
+  return res.status(200).send(data.data)
 }
--- pages/api/coins/index.js
+++ pages/api/coins/index.js
@@ -3,33 +3,36 @@ import jwt from 'jsonwebtoken';
 
 export default async function handler(req,res){
   const url = `https://cp0099.herokuapp.com/api/coins`;
+  const loc = `http://localhost:8000/api/coins`
+  const SECRET_KEY="rJGNX+ehxRvcsNEFERwU0lUbzOXsb9fXgjEIbCh5f8xh3UdYlKQ4JrLCKW0NEIFkP/aUpx6pUs6h6N98Hgu/lEfqo7BDs3EcQ1Dfu8rYr19mWzaVmOWdhSLX7PxGJFKO3hyafrw0yLOJoOZljcsyftwwWq7UafhNyhwF6PKKwk6POAUPdqZP41ux7eCSvhWTt5fWXmvldcgYemPqdY7j1FisGJq8zmP68jJ4WUUNYQy5jtYfyp0MFYrQzhHiNmX48ZkkcdeMXbybLvLBWUyK1P9+vzfotB4SNKQk5OsusI0c8sgmkOHm4G0yOv69/wwkqYmnmLtkiKjz9jjZoFLOyw=="
+    const token = jwt.sign({type:'verification'},SECRET_KEY,{algorithm:'HS256'})
+    const options = {
+      headers: {
+        'Content-Type':'application/json',
+        referer: 'https://nextjs-dusky-gamma.vercel.app/',
+        'verify-key': token
+      }
+    }
   if(req.method == 'PATCH'){
     const userData = {
       ID: req.body.ID,
       uid: req.body.uid
     }
     try {
-      const data = await axios.patch(url,userData);
+      const data = await axios.patch(url,userData,options);
       return res.status(200).send(data.data)
     } catch (err) {
       return res.status(500).send({error:"an error occurred"})
     }
   }else if(req.method == 'POST'){
     try {
-      const response = await axios.post(url,req.body)
+      const response = await axios.post(url,req.body,options)
       return res.status(200).send(response.data);
     } catch (err) {
       return res.status(500).send(response.data);
     }
-  }else{
-    const SECRET_KEY="rJGNX+ehxRvcsNEFERwU0lUbzOXsb9fXgjEIbCh5f8xh3UdYlKQ4JrLCKW0NEIFkP/aUpx6pUs6h6N98Hgu/lEfqo7BDs3EcQ1Dfu8rYr19mWzaVmOWdhSLX7PxGJFKO3hyafrw0yLOJoOZljcsyftwwWq7UafhNyhwF6PKKwk6POAUPdqZP41ux7eCSvhWTt5fWXmvldcgYemPqdY7j1FisGJq8zmP68jJ4WUUNYQy5jtYfyp0MFYrQzhHiNmX48ZkkcdeMXbybLvLBWUyK1P9+vzfotB4SNKQk5OsusI0c8sgmkOHm4G0yOv69/wwkqYmnmLtkiKjz9jjZoFLOyw=="
-    const token = jwt.sign({type:'verification'},SECRET_KEY,{algorithm:'HS256'})
-    const options = {
-      headers: {
-        referer: 'https://nextjs-dusky-gamma.vercel.app/',
-        'verify-key': token
-      }
-    }
+  }
+  else{
     const data = await axios.get(url,options)
     return res.status(200).send(data.data)
   }
