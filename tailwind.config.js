e8fe0ecfd65fb18ebb3fd63779c128cd1dc53410
xNikhil001 <naikn8113@gmail.com>
2022-05-14 13:51:57 +0530

    fixed tailwind error

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
       },
       boxShadow: {
         '3xl': 'rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset',
-        '4xl': 'rgba(0, 0, 0, 0.18) 0px 2px 4px;',
+        '4xl': 'rgba(0, 0, 0, 0.18) 0px 2px 4px',
       }
     },
   },
