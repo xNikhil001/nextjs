c032c3569c851b6c6111d642878236d1e815dc36
xNikhil001 <naikn8113@gmail.com>
2022-06-27 22:41:11 +0530

    added custom date option to form

--- pages/submit.js
+++ pages/submit.js
@@ -18,6 +18,7 @@ function Submit(){
     }
     checkSession();
   },[]);
+  const days = [...Array(31)]
   const [formData,setFormData] = useState({
     name: "",
     symbol: "",
@@ -26,11 +27,9 @@ function Submit(){
     address: "",
     website: "",
     chain: "",
-    release: {
-      year: "",
-      month: "",
-      day: ""
-    },
+    year: "",
+    month: "",
+    day: "",
     marketcap: "",
     twitter: "",
     telegram: ""
@@ -57,7 +56,24 @@ function Submit(){
   }
   function submitForm(e){
     e.preventDefault();
-    const validator = new Validate(formData)
+    const newForm = {
+      name: formData.name,
+      symbol: formData.symbol,
+      logo: formData.logo,
+      description: formData.description,
+      address: formData.address,
+      website: formData.website,
+      chain: formData.chain,
+      release: {
+        year: formData.year,
+        month: formData.month,
+        day: formData.day,
+      },
+      marketcap: formData.marketcap,
+      twitter: formData.twitter,
+      telegram: formData.telegram
+    }
+    const validator = new Validate(newForm)
     setError(validator.errors);
     if(Object.keys(validator.errors).length == 0){
       postFormData();
@@ -70,79 +86,104 @@ function Submit(){
   }
   return(
       <>
-        <form className="max-w-[700px] w-11/12 mx-auto shadow-lg">
-          <h3 className="w-full mx-auto text-2xl sm:w-11/12">Submit your coin</h3>
-            <label className="flex flex-col my-4">
+        <form className="max-w-[700px] w-11/12 mx-auto">
+          <h3 className="w-full mx-auto text-2xl">Submit your coin</h3>
+            <label className="flex flex-col mt-4">
             Coin Name
-            <input className="input" name="coin_name" type="text" value={formData.name || ""} onChange={handleForm} />
-            {error.nameError && error.nameError.map((el)=>(<div className="text-red-400 w-11/12 sm:w-10/12 mx-auto" key={Math.random()}>-{el}</div>))}
             </label>
-            <label className="flex flex-col my-4">
+            <input className="input" name="name" type="text" value={formData.name || ""} onChange={handleForm} />
+            {error.nameError && error.nameError.map((el)=>(<div className="text-red-400 w-11/12 mx-auto" key={Math.random()}>-{el}</div>))}
+            
+            <label className="flex flex-col mt-4">
             Symbol
+            </label>
             <input className="input" name="symbol" type="text" value={formData.symbol || ""} onChange={handleForm}/>
-             {error.symbolError && error.symbolError.map((el)=>(<div className="text-red-400 w-11/12 sm:w-10/12 mx-auto" key={Math.random()}>-{el}</div>))}
-             </label>
+             {error.symbolError && error.symbolError.map((el)=>(<div className="text-red-400 w-11/12 mx-auto" key={Math.random()}>-{el}</div>))}
              
-            <label className="flex flex-col my-4">
+            <label className="flex flex-col mt-4">
             Logo
+            </label>
             <input className="input" type="url" name="logo" value={formData.logo || ""} onChange={handleForm} />
-             {error.logoError && error.logoError.map((el)=>(<div className="text-red-400 w-11/12 sm:w-10/12 mx-auto" key={Math.random()}>-{el}</div>))}
-            </label>
-            
-            <label className="flex flex-col my-4">
+             {error.logoError && error.logoError.map((el)=>(<div className="text-red-400 w-11/12 mx-auto" key={Math.random()}>-{el}</div>))}
+             
+            <label className="flex flex-col mt-4">
             Description
+            </label>
             <textarea className="input" name="description" value={formData.description || ""} onChange={handleForm} cols="40" rows="6"></textarea>
-             {error.descError && error.descError.map((el)=>(<div className="text-red-400 w-11/12 sm:w-10/12 mx-auto" key={Math.random()}>-{el}</div>))}
-             </label>
+             {error.descError && error.descError.map((el)=>(<div className="text-red-400 w-11/12 mx-auto" key={Math.random()}>-{el}</div>))}
              
-            <label className="flex flex-col my-4">
+            <label className="flex flex-col mt-4">
             Contract Address
+            </label>
             <input className="input" type="text" name="address" value={formData.address || ""} onChange={handleForm} />
-             {error.addressError && error.addressError.map((el)=>(<div className="text-red-400 w-11/12 sm:w-10/12 mx-auto" key={Math.random()}>-{el}</div>))}
-            </label>
+             {error.addressError && error.addressError.map((el)=>(<div className="text-red-400 w-11/12 mx-auto" key={Math.random()}>-{el}</div>))}
              
-            <label className="flex flex-col my-4">
+            <label className="flex flex-col mt-4">
             Website Link
-            <input className="input" type="url" name="website" value={formData.website || ""} onChange={handleForm} />
-             {error.websiteError && error.websiteError.map((el)=>(<div className="text-red-400 w-11/12 sm:w-10/12 mx-auto" key={Math.random()}>-{el}</div>))}
             </label>
-            
-            <label className="flex flex-col my-4">
+            <input className="input" type="url" name="website" value={formData.website || ""} onChange={handleForm} />
+             {error.websiteError && error.websiteError.map((el)=>(<div className="text-red-400 w-11/12 mx-auto" key={Math.random()}>-{el}</div>))}
+             
+            <label className="flex flex-col mt-4">
             Block Chain
-            <select name="chain" value={formData.chain || ""} onChange={handleForm} className="bg-[#262c40] shadow-4xl rounded-sm p-2 outline-0 hover:bg-gray-500">
-              <option value="">Select Block Chain type</option>
+            </label>
+            <select name="chain" value={formData.chain || ""} onChange={handleForm} className="bg-gray-300 rounded-sm p-2 outline-0 w-full">
+              <option value=""></option>
               <option value="ETH">Ethereum</option>
               <option value="BSC">Binance Smart Chain</option>
               <option value="MATIC">Polygon</option>
               <option value="TRX">TRON</option>
               <option value="SOL">SOLANA</option>
             </select>
-             {error.chainError && error.chainError.map((el)=>(<div className="text-red-400 w-11/12 sm:w-10/12 mx-auto" key={Math.random()}>-{el}</div>))}
-            </label>
-            
-            <label className="flex flex-col my-4">
+             {error.chainError && error.chainError.map((el)=>(<div className="text-red-400 w-11/12 mx-auto" key={Math.random()}>-{el}</div>))}
+             
+            <label className="flex flex-col mt-4">
             Release date
-            <input className="input bg-white" type="date" name="release" value={formData.release || ""} onChange={handleForm} />
-             {error.releaseError && error.releaseError.map((el)=>(<div className="text-red-400 w-11/12 sm:w-10/12 mx-auto" key={Math.random()}>-{el}</div>))}
             </label>
+            <div className="grid grid-cols-3 gap-1">
+            <div className="flex flex-col">
+            <span>Day</span>
+            <select name="day" className="input" value={formData.day || ""} onChange={handleForm}>
+                <option value=""></option>
+              {days.map((el,index)=>(<option value={index+1} key={index}>{index+1}</option>))}
+            </select>
+            </div>
             
-            <label className="flex flex-col my-4">
+            <div className="flex flex-col">
+            <span>Month</span>
+            <select name="month" className="input" value={formData.month || ""} onChange={handleForm}>
+                <option value=""></option>
+              {[...Array(12)].map((el,index)=>(<option value={index+1} key={index}>{index+1}</option>))}
+            </select>
+            </div>
+            
+            <div className="flex flex-col">
+            <span>Year</span>
+            <select name="year" className="input" value={formData.year || ""} onChange={handleForm}>
+                <option value=""></option>
+              {[...Array(15)].map((el,index)=>(<option value={2008+(index+1)} key={index}>{2008+(index+1)}</option>))}
+            </select>
+            </div>
+            </div>
+             {error.releaseError && error.releaseError.map((el)=>(<div className="text-red-400 w-11/12 mx-auto" key={Math.random()}>-{el}</div>))}
+             
+            <label className="flex flex-col mt-4">
             Market Capital
+            </label>
             <input className="input" type="number" name="marketcap" value={formData.marketcap || ""} onChange={handleForm} />
-             {error.marketcapError && error.marketcapError.map((el)=>(<div className="text-red-400 w-11/12 sm:w-10/12 mx-auto" key={Math.random()}>-{el}</div>))}
-            </label>
-            
-            <label className="flex flex-col my-4">
+             {error.marketcapError && error.marketcapError.map((el)=>(<div className="text-red-400 w-11/12 mx-auto" key={Math.random()}>-{el}</div>))}
+             
+            <label className="flex flex-col mt-4">
             Twitter Link
-            <input className="input" type="url" name="twitter" value={formData.twitter || ""} onChange={handleForm} />
-             {error.twitterError && error.twitterError.map((el)=>(<div className="text-red-400 w-11/12 sm:w-10/12 mx-auto" key={Math.random()}>-{el}</div>))}
             </label>
-            
-            <label className="flex flex-col my-4">
+           <input className="input" type="url" name="twitter" value={formData.twitter || ""} onChange={handleForm} />
+             {error.twitterError && error.twitterError.map((el)=>(<div className="text-red-400 w-11/12 mx-auto" key={Math.random()}>-{el}</div>))}
+             
+            <label className="flex flex-col mt-4">
             Telegram Link
-            <input className="input" type="url" name="telegram" value={formData.telegram || ""} onChange={handleForm} />
-            {error.telegramError && error.telegramError.map((el)=>(<div className="text-red-400 w-11/12 sm:w-10/12 mx-auto" key={Math.random()}>-{el}</div>))}
             </label>
+            <input className="input" type="url" name="telegram" value={formData.telegram || ""} onChange={handleForm} />
+            {error.telegramError && error.telegramError.map((el)=>(<div className="text-red-400 w-11/12 mx-auto" key={Math.random()}>-{el}</div>))}
             
             <button className="w-6/12 sm:w-4/12 bg-[#a0dce6] shadow-3xl mx-auto block p-2 my-6 rounded-md text-black font-black h-[42px]" onClick={submitForm}>{formLoading?(<div className="flex justify-center items-center"><img src="/three-dots.svg" width="45px" height="45px" /></div>) : "Submit"}</button>
         </form>
